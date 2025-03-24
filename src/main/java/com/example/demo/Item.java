0bc69d984805b6e15d089e69f906282e2a4f7769
Ivan Nikolov <ivan.nikolov@sap.com>
2025-03-24 15:56:06 +0200

    Add base sourse

---
+++ src/main/java/com/example/demo/Item.java
@@ -0,0 +1,66 @@
+package com.example.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(generator = "increment")
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private int quantity;
+
+    public Item() {
+    }
+
+    public Item(String name, String description, double price, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
---
+++ src/main/java/com/example/demo/ItemController.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+
+    private final ItemRepository repository;
+
+    @Autowired
+    public ItemController(ItemRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Item> getItems(){
+        return repository.save();
+    }
+}
